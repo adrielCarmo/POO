29c54bb69c7d5a35247d9ae3b3f8a394eaee553a
adrielCarmo <adrieldovale7@gmail.com>
2023-08-27 23:53:29 -0400

    add BasePessoa

---
+++ lista03/src/ex001/BasePessoa.java
@@ -0,0 +1,10 @@
+package ex001;
+
+import java.util.Date;
+public abstract class BasePessoa {
+    private int codigo;
+    private String nome, endereco, telefone, rg, cpf;
+    private Date dataNascimento, dataInsercao;
+
+    
+}
