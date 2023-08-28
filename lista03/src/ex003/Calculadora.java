97a13cf707daa30f003c4cf81a14ad68716fa553
adrielCarmo <adrieldovale7@gmail.com>
2023-08-28 15:13:31 -0400

    adição a fins de teste de implementação

---
+++ lista03/src/ex003/Calculadora.java
@@ -0,0 +1,19 @@
+package ex003;
+
+public class Calculadora {
+    public int Somar(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+
+    public int Subtrair(int numero1, int numero2) {
+        return numero1 - numero2;
+    }
+
+    public int Multiplicar(int numero1, int numero2) {
+        return numero1 * numero2;
+    }
+
+    public int Dividir(int numero1, int numero2) {
+        return numero1 / numero2;
+    }
+}
