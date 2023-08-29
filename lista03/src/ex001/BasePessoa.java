4bc1653a45c473516eefca5d282fb25df7ddaccf
adrielCarmo <adrieldovale7@gmail.com>
2023-08-29 19:50:18 -0400

    atualizando a propriedade dos atributos da classe BasePessoa

--- lista03/src/ex001/BasePessoa.java
+++ lista03/src/ex001/BasePessoa.java
@@ -3,9 +3,9 @@ package ex001;
 import java.util.Date;
 
 public abstract class BasePessoa {
-    private int codigo;
-    private String nome, endereco, telefone, rg, cpf;
-    private Date dataNascimento, dataInsercao;
+    protected int codigo;
+    protected String nome, endereco, telefone, rg, cpf;
+    protected Date dataNascimento, dataInsercao;
 
     public BasePessoa() {
 
@@ -19,6 +19,70 @@ public abstract class BasePessoa {
         this.rg = rg;
         this.cpf = cpf;
         this.dataNascimento = dataNascimento;
+        this.dataInsercao = dataInsercao;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Date getDataInsercao() {
+        return dataInsercao;
+    }
+
+    public void setDataInsercao(Date dataInsercao) {
         this.dataInsercao = dataInsercao;
     }
 }
